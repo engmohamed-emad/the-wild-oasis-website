a8cf51881fb27609bc451afecc03b92ca295767d
OMDA <Mohamed.elfattah04@eng-st.cu.edu.eg>
2025-09-08 15:44:48 +0300

    nc

--- app/account/reservations/edit/[bookingId]/loading.js
+++ app/account/reservations/edit/[bookingId]/loading.js
@@ -3,5 +3,6 @@ import Spinner from "@/app/_components/Spinner";
 export default function Loading() {
   return <div>
     <Spinner />
+    <p className="text-primary-200 text-xl">Loading reservation...</p>
   </div>;
 }
