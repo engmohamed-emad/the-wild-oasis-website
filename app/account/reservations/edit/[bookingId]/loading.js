17f8acbd6513eed64071fc327fcf38e8f59981b4
OMDA <Mohamed.elfattah04@eng-st.cu.edu.eg>
2025-09-08 15:46:54 +0300

    tc

--- app/account/reservations/edit/[bookingId]/loading.js
+++ app/account/reservations/edit/[bookingId]/loading.js
@@ -1,7 +1,7 @@
 import Spinner from "@/app/_components/Spinner";
 
 export default function Loading() {
-  return <div>
+  return <div className="grid justify-center items-center h-full">
     <Spinner />
     <p className="text-primary-200 text-xl">Loading reservation...</p>
   </div>;
